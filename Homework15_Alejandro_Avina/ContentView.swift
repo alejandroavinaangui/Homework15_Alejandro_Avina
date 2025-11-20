fa58e723b0e850457a66513b1758478334ae98fd
alejandroavinaa <127714455+alejandroavinaa@users.noreply.github.com>
2025-11-19 21:01:36 -0800

    Initial Commit

---
+++ Homework15_Alejandro_Avina/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Homework15_Alejandro_Avina
+//
+//  Created by Alejandro Avina on 11/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Homework15_Alejandro_Avina/Homework15_Alejandro_AvinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homework15_Alejandro_AvinaApp.swift
+//  Homework15_Alejandro_Avina
+//
+//  Created by Alejandro Avina on 11/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Homework15_Alejandro_AvinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
